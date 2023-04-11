df9bb5122cbeb7ee6c12162233b003d5d8f65e89
silversurfer8080 <gilberto01@hotmail.com>
2023-04-10 23:33:22 -0300

    Max depth of a binary tree (the root-to-leaf path). Given a binary tree, find its max depth.

---
+++ src/MaxDephBinaryTree.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+import java.util.function.Function;
+
+public class MaxDephBinaryTree {
+    private static int dfs(Node<Integer> root) {
+        // Null node adds no depth.
+        if (root == null) return 0;
+        // Num nodes in the longest path of current subtree = max num nodes of its two subtrees + 1 current node.
+        return Math.max(dfs(root.left), dfs(root.right)) + 1;
+    }
+
+    public static int treeMaxDepth(Node<Integer> root) {
+        return (root != null) ? dfs(root) - 1 : 0;
+    }
+
+    public static class Node<T> {
+        public T val;
+        public Node<T> left;
+        public Node<T> right;
+
+        public Node(T val) {
+            this(val, null, null);
+        }
+
+        public Node(T val, Node<T> left, Node<T> right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+
+}
