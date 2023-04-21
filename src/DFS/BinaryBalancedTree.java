a309658be8e1122447bd56a4325f5514ed7636ad
silversurfer8080 <gilberto01@hotmail.com>
2023-04-21 15:07:27 -0300

    Balanced Binary Tree.

--- src/DFS/BinaryBalancedTree.java
+++ src/DFS/BinaryBalancedTree.java
@@ -3,21 +3,24 @@ package DFS;
 public class BinaryBalancedTree {
     // Returns -1 if is not a balanced binary tree. The height if it is.
     public static int treeHeight(Node<Integer> tree) {
-        if (tree == null) {
-            return 0;
-        }
-        int leftHeight = treeHeight(tree.left);
-        int rightHeight = treeHeight(tree.right);
-        if (leftHeight == -1 || rightHeight == -1) {
-            return -1;
-        }
-        if (Math.abs(leftHeight - rightHeight) > 1 ) {
-            return -1;
-        }
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
+        if(tree == null) return 0;
 
-    public static boolean isBalanced(Node<Integer> tree) {
+        int leftSubTreeHeight = treeHeight(tree.left);
+        //if left subTree is not balanced then the entire tree is also not balanced.
+        if(leftSubTreeHeight == -1) return -1;
+
+        int rithSubTreeHeight = treeHeight(tree.right);
+        //if right subTree is not balanced then the entire tree is also not balanced.
+        if(rithSubTreeHeight == -1) return -1;
+
+        // Checking the difference of left and right subtree. for current node.
+        if(Math.abs(leftSubTreeHeight - rithSubTreeHeight) > 1) return -1;
+
+        // If it's balanced then return the height
+        return (Math.max(leftSubTreeHeight, rithSubTreeHeight) + 1);
+
+    }
+    private static boolean isBalanced(Node<Integer> tree) {
         return treeHeight(tree) != -1;
     }
 
