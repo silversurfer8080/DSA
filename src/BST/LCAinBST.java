4c7792dbdd3fcb039c6757cd8d0dc1ffe72ec23b
silversurfer8080 <gilberto01@hotmail.com>
2023-05-10 23:56:07 -0300

    Lowest common ancestor on Binary Search Tree

--- src/BST/LCAinBST.java
+++ src/BST/LCAinBST.java
@@ -5,8 +5,12 @@ import DFS.Node;
 public class LCAinBST {
 
     public static int lcaOnBST(Node<Integer> node, int p, int q){
-
-
-        return 0;
+        if (p < node.val &&  q < node.val) {
+            return lcaOnBST(node.left, p, q);
+        } else if (p > node.val && q > node.val) {
+            return lcaOnBST(node.right, p, q);
+        } else {
+            return node.val;
+        }
     }
 }
