55c62d8ed7a858f8a062e9afc8d116b2406f0715
silversurfer8080 <gilberto01@hotmail.com>
2023-05-12 19:21:19 -0300

    Starting simples tests with numbers and Strings

---
+++ src/playground/numbersPlayground.java
@@ -0,0 +1,71 @@
+package playground;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class numbersPlayground {
+
+    public static int largestNumber(int[] nums){
+        int maxResult = nums[0];
+        for (int i = 1; i < nums.length; i++){
+            if(maxResult < nums[i]){
+                maxResult = nums[i];
+            }
+        }
+        return maxResult;
+    }
+
+    public static boolean primeNumber(int num){
+        boolean isPrime = true;
+        for(int i = 2; i<= Math.sqrt(num); i++){
+            if(num % i == 0){
+                isPrime = false;
+            }
+        }
+        return isPrime;
+    }
+
+    public static int[] removeDuplicates(int[] nums){
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){
+            set.add(num);
+        }
+        return set.stream().mapToInt(Number::intValue).toArray();
+    }
+
+    public static int secondLargestNumber(int[] nums){
+        int firstMax =  Integer.MIN_VALUE;
+        int secondMax =  Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++){
+            if(nums[i] > firstMax){
+                secondMax = firstMax;
+                firstMax = nums[i];
+            } else if(nums[i] > secondMax && nums[i] < firstMax){
+                secondMax = nums[i];
+            }
+        }
+        return secondMax;
+    }
+
+    public static int secondLargestNumber2(int[] nums){
+        int result = -1;
+        if(nums.length <2) return result;
+        Arrays.sort(nums);
+        for(int i = nums.length -1; i >= 0; i--){
+            if(nums[i] != nums[nums.length-1]) result = nums[i];
+        }
+        return result;
+    }
+    public static int secondLargestNumber3(int[] nums){
+        List<Integer> list = Arrays.stream(nums).boxed().toList();
+        Collections.sort(list);
+        return list.get(1);
+    }
+
+
+
+
+
+
+}
