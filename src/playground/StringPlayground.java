927a3a4ac4791690cc0d944448feeecc31e68bd1
silversurfer8080 <gilberto01@hotmail.com>
2023-05-13 10:09:49 -0300

    More simple algos

--- src/playground/StringPlayground.java
+++ src/playground/StringPlayground.java
@@ -1,5 +1,10 @@
 package playground;
 
+import java.text.StringCharacterIterator;
+import java.util.*;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 public class StringPlayground {
 
     public static String reverseString(String stringueira){
@@ -24,6 +29,47 @@ public class StringPlayground {
         }
         return reverseString;
     }
+
+    public static boolean isAnagram(String word1, String word2){
+        boolean result = false;
+        char[] arr1 = word1.toCharArray();
+        char[] arr2 = word2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1,arr2)) result = true;
+
+        return result;
+   }
+    public static boolean isAnagram1(String word1, String word2){
+        boolean result = false;
+        if(word1 == null  || word2 == null) return false;
+        if(word1.length() != word2.length()) return false;
+
+        int hash1[] = new int[26];
+        int hash2[] = new int[26];
+
+        for (int i = 0; i < word1.length(); i++) {
+            hash1[i] =word1.charAt(i)-'a';
+            hash2[i] =word2.charAt(i)-'a';
+        }
+
+        if(hash1.equals(hash2)) result = true;
+
+        return result;
+   }
+    public static boolean isPalindrome1(String word1){
+        boolean result = false;
+        String palindromeWord1 = null;
+        for (int i = word1.length()-1; i >= 0; i--) {
+            palindromeWord1 += word1.charAt(i);
+        }
+        if(word1.equals(palindromeWord1)) result = true;
+        return result;
+   }
+
+
 
 
 
