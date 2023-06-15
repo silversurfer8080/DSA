d3d3ebf6793aa76cf8073cf65028b2b9b8360fea
silversurfer8080 <gilberto01@hotmail.com>
2023-06-15 12:09:34 -0300

    Templates? Backtracking.

--- src/Templates/Backtracking1.java
+++ src/Templates/Backtracking1.java
@@ -1,5 +1,23 @@
 package Templates;
 
-public class Backtracking1 {
+import java.util.List;
 
+public class Backtracking1 {
+//    private static void dfs(int startIndex, List<T> path, List<List<T>> res, [...additional states]) {
+//        if (isLeaf(startIndex)) {
+//            res.add(new ArrayList<>(path)); // add a copy of the path to the result
+//            return;
+//        }
+//        for (T edge : getEdges(startIndex, [...additional states])) {
+//            path.add(choice);
+//            if (...additionl statees) update(...additional states)
+//            dfs(startIndex + edge.length(), res, [...additional states]);
+//            path.remove(path.size() - 1);
+//            // revert(...additional states) if necessary e.g. permutations
+//        }
+//    }
+/**
+ *  The main difference here is we are keeping one or more additional states as dfs parameters. And we need update those states when update
+ *  start_index.
+  */
 }
