a3ae44693e5d86b8b354ada16017640e0acf6b37
silversurfer8080 <gilberto01@hotmail.com>
2023-06-26 11:12:39 -0300

    Generate all permutations in given String.

---
+++ src/Backtracking/LettersPermutation.java
@@ -0,0 +1,35 @@
+package Backtracking;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Given a string of unique letters, find all of tis distinct permutations.
+public class LettersPermutation {
+    public static List<String> permutations(String s) {
+        char[] letters = s.toCharArray();
+        List<String> res = new ArrayList<>();
+        dfs(0, new ArrayList<>(), new boolean[letters.length], res, letters);
+        return res;
+    }
+
+    private static void dfs(int startIndex, ArrayList<Object> path, boolean[] used, List<String> res, char[] letters) {
+        if(startIndex == used.length){
+            // Make a deep copy since otherwise we'd be append the same list over and over.
+            res.add(path.stream().map(e -> e.toString()).collect(Collectors.joining()));
+            return;
+        }
+
+        for (int i = 0; i < used.length; i++){
+            // Skip used letters
+            if(used[i]) continue;
+            // Add letter to permutation, mark the letter as used
+            path.add(letters[i]);
+            used[i] = true;
+            dfs(startIndex+1, path, used, res, letters);
+            path.remove(path.size() - 1);
+            used[i] = false;
+        }
+    }
+
+}
