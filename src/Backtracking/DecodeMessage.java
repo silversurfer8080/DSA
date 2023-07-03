b882201203c614f9d7bb96463ab1fa3e1fcc60ae
silversurfer8080 <gilberto01@hotmail.com>
2023-07-02 21:13:58 -0300

    Decode message options.

---
+++ src/Backtracking/DecodeMessage.java
@@ -0,0 +1,15 @@
+package Backtracking;
+// We have a message consisting of digits 0-9 to decode.
+// Letters are encoded to digits by their position in the alphabet.
+// A -> 1, B -> 2, C -> 3... y -> 25, z -> 26.
+
+public class DecodeMessage {
+    // Given a non-empty string of digits, how many ways are there to decode it?
+    // Example: Input: 18 Output: 2, 18 can be decoded as "AH" or "R".
+    public static int decodeWays(String digits){
+
+
+        return 0;
+    }
+
+}
