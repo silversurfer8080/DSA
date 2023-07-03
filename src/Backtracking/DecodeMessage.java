d109c1d2197c9a7bb948693a2e88fa4445ae2f4a
silversurfer8080 <gilberto01@hotmail.com>
2023-07-03 10:39:10 -0300

    Decode message options.

--- src/Backtracking/DecodeMessage.java
+++ src/Backtracking/DecodeMessage.java
@@ -7,9 +7,23 @@ public class DecodeMessage {
     // Given a non-empty string of digits, how many ways are there to decode it?
     // Example: Input: 18 Output: 2, 18 can be decoded as "AH" or "R".
     public static int decodeWays(String digits){
+        return dfs(0, digits);
+    }
 
+    // The time complexity overall is O(n²).
+    private static int dfs(int startIndex, String digits) {
+        if(startIndex == digits.length()) return 1;
 
-        return 0;
+        int ways = 0;
+        // Can not decode string with leading 0
+        if(digits.charAt(startIndex) == '0') return ways;
+        // Decode one digit
+        ways += dfs(startIndex + 1, digits);
+        // Decode two digits
+        if(startIndex + 2 <= digits.length() && Integer.parseInt(digits.substring(startIndex, startIndex + 2)) <= 26) {
+            ways += dfs(startIndex + 2, digits);
+        }
+        return ways;
     }
 
 }
