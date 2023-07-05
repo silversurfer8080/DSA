518fe4e23b8730d025e1e0165968843155adc63b
silversurfer8080 <gilberto01@hotmail.com>
2023-07-04 21:16:57 -0300

    Minimum coins change

---
+++ src/Backtracking/MinCoinsChange.java
@@ -0,0 +1,13 @@
+package Backtracking;
+
+import java.util.List;
+
+// You are given coins of denominations and an amount. Write a function to compute the fewest number of coins
+// that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+public class MinCoinsChange {
+    public static  int coinChange(List<Integer> coins, int amount){
+
+
+        return 0;
+    }
+}
