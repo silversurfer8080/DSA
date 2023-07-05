deefa934b515b27d9f10c5f14919591c813ef791
silversurfer8080 <gilberto01@hotmail.com>
2023-07-04 21:21:57 -0300

    Minimum coins change implementation.

--- src/Backtracking/MinCoinsChange.java
+++ src/Backtracking/MinCoinsChange.java
@@ -6,8 +6,22 @@ import java.util.List;
 // that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
 public class MinCoinsChange {
     public static  int coinChange(List<Integer> coins, int amount){
+        int result = minCoinsChange(coins, amount, 0);
+        return result == Integer.MAX_VALUE ? -1 : result;
 
+    }
 
-        return 0;
+    private static int minCoinsChange(List<Integer> coins, int amount, int sum) {
+        if(sum == amount) return 0;
+        if(sum > amount) return Integer.MAX_VALUE;
+        int ans = Integer.MAX_VALUE;
+        for(int coin : coins){
+            int result = minCoinsChange(coins, amount, sum + coin);
+            if(result == Integer.MAX_VALUE){
+                continue;
+            }
+            ans = Math.min(ans, result);
+        }
+        return ans;
     }
 }
