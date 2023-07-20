0faaf6d7c57e0b8548aa5584103a6100ddcb717e
silversurfer8080 <gilberto01@hotmail.com>
2023-07-20 10:47:30 -0300

    Subset of list

--- src/Backtracking/Subset2.java
+++ src/Backtracking/Subset2.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Given a set of distinct integers, nums, return all possible subsets (the power set).
-public class Subsets {
-    public static List<List<Integer>> subsets(List<Integer> nums) {
+public class Subset2 {
+    public static List<List<Integer>> subsets(List<Integer> nums){
         List<List<Integer>> res = new ArrayList<>();
         List<Integer> seq = new ArrayList<>();
         dfs(nums, res, seq, 0);
@@ -13,12 +13,11 @@ public class Subsets {
     }
 
     private static void dfs(List<Integer> nums, List<List<Integer>> res, List<Integer> seq, int i) {
-        if( i == nums.size()) {
-            res.add(new ArrayList<>(seq));
-        }
-            seq.add(nums.get(i));
-            dfs(nums, res, seq, i + 1);
+        res.add(new ArrayList<>(seq));
+        for (int j = i; j < nums.size(); j++) {
+            seq.add(nums.get(j));
+            dfs(nums, res, seq, j + 1);
             seq.remove(seq.size() - 1);
-            dfs(nums, res, seq, i + 1);
+        }
     }
 }
