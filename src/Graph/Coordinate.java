c3b6e80d01b7deff4143c18ab07e3493540f399b
silversurfer8080 <gilberto01@hotmail.com>
2023-10-11 14:51:13 -0300

    Graph, Coordinate class for Flood Fill problem

---
+++ src/Graph/Coordinate.java
@@ -0,0 +1,12 @@
+package Graph;
+
+public class Coordinate {
+    //represent the row coordinate
+    int r;
+    //represent the column coordinate
+    int c;
+    public Coordinate(int r, int c){
+        this.r = r;
+        this.c = c;
+    }
+}
