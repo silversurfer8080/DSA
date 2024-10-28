2d4282a9808159fe73e2f2bc0632c6da983520d0
silversurfer8080 <gilberto01@hotmail.com>
2024-10-28 19:18:16 +0000

    back.

--- src/Graph/Coordinate.java
+++ src/Graph/Coordinate.java
@@ -1,5 +1,7 @@
 package Graph;
 
+import java.util.Objects;
+
 public class Coordinate {
     //represent the row coordinate
     int r;
@@ -8,5 +10,19 @@ public class Coordinate {
     public Coordinate(int r, int c){
         this.r = r;
         this.c = c;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(r, c);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if ((obj instanceof Coordinate) && ((Coordinate) obj).r == r && ((Coordinate) obj).c == c) {
+            return true;
+        }
+        return false;
     }
 }
